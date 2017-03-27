962cfbd9a7e2ea8d5f2bc21e15def16bf3ad4a74
Jorge Guberte <jorgeguberte@gmail.com>
2017-03-27 02:05:08 -0300

    Added sensor reading and data structuring.Ready to talk to broker

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
 "use strict";
 
+/* Thin Client */
+class ThinClient{
+  constructor(){
+    console.log("ThinClient initialized");
+    this.things = [];
+  }
+
+  addThing(thing){
+    this.things.push(thing);
+  }
+}
+
+
+/* Thing */
+class Thing{
+  constructor(name, description, location){
+    this.name = name;
+    this.description = description;
+    this.location = location;
+    this.dataSources = {};
+  }
+
+  addDataSource(dataSource){
+    this.dataSources[dataSource.id] = dataSource;
+  }
+
+  updateDataSource(dataSource){
+    this.dataSources[dataSource.id] = dataSource.value;
+  }
+}
+
+
+
+
 var five = require('johnny-five'),
+    arduino = new five.Board();
 
-board = new five.Board();
+  var thinClient = new ThinClient();
 
-board.on('ready', function(){
-  this.samplingInterval(1000); //Change the sampling interval so we can send less data per minute to the server #TODO: Find out if this can be used in the sensor's scope
-  var ldrSensor = new five.Sensor('A0'); //Setup our LDR sensor
+arduino.on('ready', function(){
+  var thing = new Thing({name:'Vaso', description: 'Um vaso de manjericão', location: 'São Paulo - SP - Brasil'});
 
-  ldrSensor.on('change', function(value){
-    console.log(value); //Just the data for now
-  })
+  /*Setup data sources*/
+
+  var humiditySensor = new five.Sensor({pin: 'A2', freq: 500});
+  var humidity_dataSource = {id: 'humidity', value:0, name: 'Humidity', description: 'Humidity in the soil where the plant lives. Ideal number is ~75%'}
+  thing.addDataSource(humidity_dataSource);
+
+  humiditySensor.on('change', function(){
+    var humidity = this.scaleTo(100, -1); //#TODO: find out if -1 is the best lower threshold
+    thing.updateDataSource({'property':'humidity', 'value':humidity});
+  });
+
+
+
+
+  var luminositySensor = new five.Sensor({pin:'A0', freq: 500});
+  var luminosity_dataSource = {id: 'luminosity', value: 0, name: 'Luminosity', description: 'Luminosity level in the area around the plant'};
+  thing.addDataSource(luminosity_dataSource);
+
+  luminositySensor.on('change', function(){
+    var luminosity = this.scaleTo(0,100);
+    thing.updateDataSource({'property':'luminosity', 'value':luminosity });
+  });
 });
